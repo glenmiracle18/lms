2b69fab50c88b8c0d4b8797c68c387bff6e1d076
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-22 02:37:15 +0200

    build: done with authentication using clerk

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <UserButton afterSignOutUrl='/'/>
+    )
+}
+
+export default Dashboard
