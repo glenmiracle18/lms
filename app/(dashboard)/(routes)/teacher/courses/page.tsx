5ce3fcfc6a1bbfc4f3ab6b32778918a0da221edc
glenmiracle18 <m.bonyu@alustudent.com>
2024-02-06 11:47:38 +0200

    build: building the course creation ui

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const TeacherRoute = () => {
-    return (
-        <div>TeacherRoute from tsx component</div>
-    )
-}
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
+};
 
-export default TeacherRoute
+export default TeacherRoute;
--- components/navbar_routes.tsx
+++ components/navbar_routes.tsx
@@ -1,42 +1,36 @@
-"use client"
-import { UserButton } from '@clerk/clerk-react'
-import { useRouter, usePathname } from 'next/navigation';
-import { Button } from '@/components/ui/button'
-import { LogOut } from 'lucide-react';
-import  Link  from 'next/link'
-
+"use client";
+import { UserButton } from "@clerk/clerk-react";
+import { useRouter, usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    // verification for teacher / player (course) mode
-    const isTeacherPage = pathname?.startsWith('/teacher')
-    const isPlayerPage = pathname?.includes('/chapter');
+  // verification for teacher (course) mode
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
 
-    return (
-        <div className='flex gap-2 ml-auto'>
-            {
-                isTeacherPage || isPlayerPage ?
-                (
-                    <Link href="/">
-                        <Button size='sm' variant='outline'>
-                            <LogOut className='h-4 w-4 mr-2' />
-                            Exit
-                        </Button>
-                    </Link>
-                )
-                :
-                (
-                    <Link href="/teacher/courses">
-                        <Button size='sm' variant='outline'>
-                            Teacher Mode
-                        </Button>
-                    </Link>
-                )
-            }
-            <UserButton afterSignOutUrl='/'/>
-        </div>
-    )
-}
+  return (
+    <div className="ml-auto flex gap-2">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="outline">
+            <LogOut className="mr-2 h-4 w-4" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="outline">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
 
-export default NavbarRoutes
+export default NavbarRoutes;
