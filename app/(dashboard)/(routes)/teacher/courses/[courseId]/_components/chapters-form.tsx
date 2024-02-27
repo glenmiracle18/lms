90309641ce0cf061eaf41f6992595c475958df53
glenmiracle18 <m.bonyu@alustudent.com>
2024-02-27 03:59:24 +0200

    build: Chapter Reorder functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -14,11 +14,12 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { cn } from "@/lib/utils";
 import { Course, Chapter } from "@prisma/client";
+import { ChapterList } from "./chapter-list";
 
 // interface props
 interface ChaptersFormProps {
@@ -38,6 +39,7 @@ const ChaptersForm = ({
 }: ChaptersFormProps) => {
     // state for editing mode
     const [isCreating, setIsCreating] = useState(false);
+    const [isUpdating, setIsUpdating ] = useState(false);
     const toggleCreating = () => setIsCreating((current) => !current);
 
     // form hook
@@ -64,8 +66,31 @@ const ChaptersForm = ({
         }
     }
 
+    const onReorder = async (updateData: { id: string; position: number }[]) => {
+        try {
+            setIsUpdating(true)
+
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {list: updateData });
+            toast.success("Chapters reordered");
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
+    const onEdit = (id: string) => {
+        router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+    }
+
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4 ">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4 ">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Create a chapter
                 <Button variant="ghost" onClick={toggleCreating}>
@@ -115,6 +140,11 @@ const ChaptersForm = ({
                     {!initialData.chapters.length && "No Chapters!"}
 
                     {/* TODO: Add a list of chapters */}
+                    <ChapterList
+                        onEdit={onEdit}
+                        onReorder={onReorder}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (
---
+++ app/api/courses/[courseId]/chapters/reorder/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function PUT(
+    req: Request,
+    { params }: { params: { courseId: string } }
+) {
+    try {
+        const { userId } = auth()
+        const { list } = await req.json()
+
+        if(!userId) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        // checking if the current user is the course owner
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId,
+            }
+        })
+
+        if(!courseOwner) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        // updating the chapters order
+        for (let item of list) {
+            await db.chapter.update({
+                where: { id: item.id },
+                data: { position: item.position }
+            })
+        }
+
+
+        // returning the reorder status
+        return NextResponse.json("Succes", { status: 200 })
+
+    } catch (error) {
+        console.log("COURSE_ID_CHAPTER: ", error)
+        return new NextResponse("Internal Server Error", { status: 500 })
+    }
+}
