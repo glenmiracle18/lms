e106c7da5b09323603e0a1631d05a36ffd2e9edf
glenmiracle18 <m.bonyu@alustudent.com>
2024-03-11 13:43:18 +0200

    build: data table in course section

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,31 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import React from "react";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 
-const TeacherRoute = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
 
-export default TeacherRoute;
+export default CoursesPage;
