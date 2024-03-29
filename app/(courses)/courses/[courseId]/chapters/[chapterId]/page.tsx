b09989d29a8c51844d6d87084909a01e2e246613
glenmiracle18 <m.bonyu@alustudent.com>
2024-03-29 12:12:56 +0200

    chore/refactor: video hsl and description added, mux-player version upadated from 2.3.3 to 2.4.0

--- app/(courses)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(courses)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,108 @@
-const Chapter = () => {
-  return <div>chapter content</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { File } from "lucide-react";
+import { getChapter } from "@/actions/get-chapters";
+import { Banner } from "@/app/(dashboard)/_components/banner";
+import { VideoPlayer } from "./_components/video-player";
+import { CourseProgressButton } from "./_components/course_progress-button";
+import { CourseEnrollButton } from "./_components/course-enroll-button";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+  });
+
+  const isLocked = !chapter?.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+      <div className="mx-auto flex max-w-4xl flex-col pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-between p-4 md:flex-row">
+            <h2 className="mb-2 text-2xl font-semibold">{chapter.title}</h2>
+            {purchase ? (
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+              />
+            ) : (
+              <CourseEnrollButton
+                courseId={params.courseId}
+                price={course.price!}
+              />
+            )}
+          </div>
+          <Separator />
+          <div>
+            <Preview value={chapter.description!} />
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    target="_blank"
+                    key={attachment.id}
+                    className="flex w-full items-center rounded-md border bg-sky-200 p-3 text-sky-700 hover:underline"
+                  >
+                    <File />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Chapter;
+export default ChapterIdPage;
