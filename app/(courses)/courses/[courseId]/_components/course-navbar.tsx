de87e5b17143b30fd6b00bf299829b1d8df50a51
glenmiracle18 <m.bonyu@alustudent.com>
2024-03-30 07:15:15 +0200

    refactor: fixed main bugs by reinstalling node version, node modules, and re-running npm i

--- app/(courses)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(courses)/courses/[courseId]/_components/course-navbar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 
 import { NavbarRoutes } from "@/components/navbar_routes";
@@ -17,7 +16,7 @@ interface CourseNavbarProps {
 export const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="flex h-full items-center border-b bg-white p-4 shadow-sm">
-      <CourseMobileSideBar course={course} progressCount={progressCount} />
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
       <NavbarRoutes />
     </div>
   );
