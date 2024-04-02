5a1a61fc7cb1b567657787b2d6e7bb0bdcb338d5
glenmiracle18 <m.bonyu@alustudent.com>
2024-04-02 19:29:17 +0200

    refactor: changing the interface props for the course sidebar mobile.tsx

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -1,31 +1,29 @@
 "use client";
 
-import { IconType } from "react-icons/lib";
-import { cn } from "@/lib/utils";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import qs from "query-string";
+import { IconType } from "react-icons";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 interface CategoryItemProps {
-  key: String;
-  value?: String;
+  label: string;
+  value?: string;
   icon?: IconType;
-  label: String;
 }
+
 export const CategoryItem = ({
-  key,
+  label,
   value,
   icon: Icon,
-  label,
 }: CategoryItemProps) => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  // getting the currentId and the title from the url parameter after pushing to determinethe isSelected state.
   const currentCategoryId = searchParams.get("categoryId");
   const currentTitle = searchParams.get("title");
 
-  // the selection state is based on if currentCategoryId is equal to the value (item.id) => item is the category
   const isSelected = currentCategoryId === value;
 
   const onClick = () => {
@@ -45,11 +43,11 @@ export const CategoryItem = ({
 
   return (
     <button
+      onClick={onClick}
       className={cn(
         "flex items-center gap-x-1 rounded-full border border-slate-200 px-3 py-2 text-sm transition hover:border-sky-700",
         isSelected && "border-sky-700 bg-sky-200/20 text-sky-800",
       )}
-      onClick={onClick}
       type="button"
     >
       {Icon && <Icon size={20} />}
