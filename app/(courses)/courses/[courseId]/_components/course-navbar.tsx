808fd16c2f9bd9193389c5c0634a5686c216ddcc
glenmiracle18 <m.bonyu@alustudent.com>
2024-04-02 17:57:02 +0200

    refactor: solving some deployment issues at course-navbar.tsx

--- app/(courses)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(courses)/courses/[courseId]/_components/course-navbar.tsx
@@ -1,14 +1,12 @@
-import { Chapter, Course, UserProgress } from "@prisma/client";
-
 import { NavbarRoutes } from "@/components/navbar_routes";
-
+import { Chapter, Course, UserProgress } from "@prisma/client";
 import CourseMobileSideBar from "./course-mobile-sdiebar";
 
 interface CourseNavbarProps {
   course: Course & {
-    chapters: (Chapter & {
-      userProgress: UserProgress[] | null; // Make userProgress optional
-    })[];
+    chapters: Chapter & {
+      userProgress: UserProgress[] | null;
+    };
   };
   progressCount: number;
 }
@@ -16,7 +14,7 @@ interface CourseNavbarProps {
 export const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="flex h-full items-center border-b bg-white p-4 shadow-sm">
-      <CourseMobileSideBar progressCount={progressCount} course={course} />
+      <CourseMobileSideBar course={course} progressCount={progressCount} />
       <NavbarRoutes />
     </div>
   );
