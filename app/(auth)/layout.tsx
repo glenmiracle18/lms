e4f10d7da20830793758f8ffa06767d8433f87df
glenmiracle18 <m.bonyu@alustudent.com>
2024-04-02 19:43:29 +0200

    refactor: clearing multiple clerk provider usage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,8 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <div className="flex h-full items-center justify-center">{children}</div>
-    </ClerkProvider>
+    <div className="flex h-full items-center justify-center">{children}</div>
   );
 };
 
